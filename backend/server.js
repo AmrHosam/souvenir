6e14f13af94913dde9196c9c81ce77d1ab6a5f55
omarnagah <omarnagah4@gmail.com>
2022-01-14 14:58:45 +0200

    Add database Configuration

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 
 dotenv.config()
 
@@ -7,6 +8,8 @@ const app = express()
 //Set Port
 const port = process.env.PORT || 5000
 app.listen(port,console.log(`Server running in ${process.env.NODE_ENV} on ${port}`))
+//Connect to database
+connectDB()
 
 app.get('/',(req,res) => {
     res.send(`app is running on ${port}`)
