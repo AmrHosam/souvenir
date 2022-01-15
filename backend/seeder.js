12c27988bbca6a77b8105c7081496d3c3657b3c9
AmrHosam <amrhossam97@gmail.com>
2022-01-15 03:49:34 +0200

    Add dummy data on database for product model

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,4 +1,6 @@
 import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import products from './data/products.js'
 import users from "./data/users.js";
 import connectDB from "./config/db.js";
 import dotenv from 'dotenv'
@@ -8,22 +10,27 @@ connectDB()
 const importData = async () =>  {
     try{
         await User.deleteMany()
+        await Product.deleteMany()
         const createdUsers = await User.insertMany(users)
+        await Product.insertMany(products)
         console.log("data imported")
         process.exit()
     }
     catch(error){
         console.log(error)
+        process.exit(1)
     }
 }
 const destroyData = async () =>  {
     try{
         await User.deleteMany()
+        await Product.deleteMany()
         console.log("data destroyed")
         process.exit()
     }
     catch(error){
         console.log(error)
+        process.exit(1)
     }
 }
 if (process.argv[2] === '-d'){
