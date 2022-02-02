13a9d2545cd38255ce8d2198639e482b3d7accda
AmrHosam <amrhossam97@gmail.com>
2022-02-02 14:23:01 +0200

    Add redux for adding cart items to DB

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 import {
     CART_ADD_ITEM,
+    CART_ADD_ITEM_DB_REQUEST,
+    CART_ADD_ITEM_DB_SUCCESS,
+    CART_ADD_ITEM_DB_FAIL,
     CART_REMOVE_ITEM,
     CART_SAVE_SHIPPING_ADDRESS,
     CART_SAVE_PAYMENT_METHOD
@@ -21,6 +24,30 @@ export const addItem = (id, quantity) => async (dispatch, getState) => {
         }
     })
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const addItemDB = (userId, itemId, quantity) => async(dispatch) => {
+    try {
+        dispatch({
+            type: CART_ADD_ITEM_DB_REQUEST
+        })
+        await dispatch(addItem(itemId, quantity))
+        const { data } = await axios.post(`/cart/${itemId}`, { quantity, userId})
+        dispatch({
+            type: CART_ADD_ITEM_DB_SUCCESS,
+            payload: data,
+        })
+    } catch (error) {
+        dispatch(removeItem(itemId))
+        dispatch({
+            type: CART_ADD_ITEM_DB_FAIL,
+            payload:
+              error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+          });
+    }
+    
 }
 
 export const removeItem = (id) => (dispatch, getState) => {
