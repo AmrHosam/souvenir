6a032cd25a2809d75a3ff65fadc51301f53162d3
AmrHosam <amrhossam97@gmail.com>
2022-02-04 01:16:03 +0200

    Add functionality to add to cart icon in the shop screen

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
+import { addItem, addItemDB } from '../actions/cartActions';
 
 const Product = ({ product }) => {
+    const dispatch = useDispatch()
+    const { userLogin } = useSelector(state => state)
+
+    const loggedIn = userLogin.user? true : false
+    const userId = userLogin.user? userLogin.user._id : ''
+    const addtoCart = (id) => {
+        console.log(id)
+        if(loggedIn)
+            dispatch(addItemDB(userId, id, 1))
+        else
+            dispatch(addItem(id, 1))
+    }
     return (
         <Card className="my-3 p-3 rounded">
             <Link to={`/shop/${product._id}`}>
@@ -17,7 +31,7 @@ const Product = ({ product }) => {
                             <strong>{product.name}</strong>
                         </div>
                     </Link>
-                    <Link to={`/add${product._id}`}>
+                    <Link to={'/cart'} onClick={() => (product.countInStock > 0) && addtoCart(product._id)}>
                         <i className="far fa-bookmark fa-2x" style={{float: "right"}}></i>
                     </Link>
                 </Card.Title>
