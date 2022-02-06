a115e06ab199525ff424908631d36388fc331eb8
AmrHosam <amrhossam97@gmail.com>
2022-02-06 11:33:23 +0200

    Add api for creating a review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -27,7 +27,39 @@ const getProductById = asyncHandler(async(req, res) => {
         res.status(404, json({message: "Product not found"}))
 })
 
+  // @desc    Create a new review
+  // @route   POST /shop/:id/reviews
+  // @access  Private
+  const createProductReview = asyncHandler(async (req, res) => {
+    const { rating, comment } = req.body
+    
+    const product = await Product.findById(req.params.id)
+
+    if(product){
+        const alreadyReviewed = product.reviews.find((rev) => rev.user.toString() === req.user._id.toString())
+        if(alreadyReviewed){
+            res.status(400)
+            throw new Error('Product already reviewed')
+        }
+        const review = {
+            name: req.user.name,
+            rating: Number(rating),
+            comment,
+            user: req.user._id
+        }
+        product.reviews.push(review)
+        product.numReviews = product.reviews.length
+        product.rating = product.reviews.reduce((acc, rev) => acc + rev.rating, 0) / product.reviews.length
+        await product.save()
+        res.status(201).json({ message: 'Review added '})
+    }else{
+        res.status(404)
+        throw new Error('Product not found')
+    }
+  })
+
 export {
     getProducts,
-    getProductById
+    getProductById,
+    createProductReview,
 }
