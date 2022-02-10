4187e8a581a29e20f1f077d4dc0622ffd3167074
omarnagah <omarnagah4@gmail.com>
2022-02-10 15:25:01 +0200

    Add redux to get all orders for admin

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -2,8 +2,12 @@ import axios from "axios";
 import {
     ORDER_CREATE_FAIL,
     ORDER_CREATE_REQUEST,
-    ORDER_CREATE_SUCCESS
+    ORDER_CREATE_SUCCESS,
+    ORDER_LIST_FAIL,
+    ORDER_LIST_REQUEST,
+    ORDER_LIST_SUCCESS
 } from "../constants/orderConstants";
+import { logout } from "./userActions";
 
 export const createOrder = (order) => async (dispatch, getState) => {
     try {
@@ -28,3 +32,40 @@ export const createOrder = (order) => async (dispatch, getState) => {
     }
 }
 
+export const listOrders = () => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_LIST_REQUEST,
+      })
+  
+      const {
+        userLogin: { user },
+      } = getState()
+  
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      }
+  
+      const { data } = await axios.get(`/orders/listAll`, config)
+  
+      dispatch({
+        type: ORDER_LIST_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: ORDER_LIST_FAIL,
+        payload: message,
+      })
+    }
+  }
+  
