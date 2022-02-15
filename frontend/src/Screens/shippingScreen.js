8f6124e35502881a043840777401575b4b4e9ad7
AmrHosam <amrhossam97@gmail.com>
2022-02-15 21:11:32 +0200

    Ask the user to login before making an order

--- frontend/src/Screens/shippingScreen.js
+++ frontend/src/Screens/shippingScreen.js
@@ -1,11 +1,12 @@
 
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../actions/userActions";
 import Message from "../Components/Message";
 import CheckoutSteps from "../Components/CheckoutSteps";
+import LoginScreen from '../Screens/LoginScreen'
 import "../Components/MyForm.css";
 import _ from "lodash";
 import { shippingAddress } from "../actions/cartActions";
@@ -17,6 +18,9 @@ const ShippingScreen = ({ history }) => {
     const [address, setAddress] = useState(shippingAddress.address);
     const [city, setCity] = useState(shippingAddress.city);
     const [phone, setPhone] = useState(shippingAddress.phone);
+
+    const location = useLocation()
+
     const Navigate = useNavigate();
     const userLogin = useSelector((state) => state.userLogin);
     const { loading, user, error } = userLogin;
@@ -39,62 +43,59 @@ const ShippingScreen = ({ history }) => {
         dispatch(saveShippingAddress({ address, city, phone }));
         Navigate('/payment')
     };
-    // useEffect(() => {
-    //     if (_.has(user, "_id")) {
-    //         console.log("wasal");
-    //         Navigate("/");
-    //     } else Navigate("/login");
-    // }, [user]);
     return (
-
-
-        <Form className="m-5" onSubmit={submitHandler}>
+        <>
+        {!user?(<><CheckoutSteps step1/><LoginScreen replace state={{from : location.pathname}}/></>):(
+        <>
             <CheckoutSteps step1 step2 />
+            <Form className="m-5" onSubmit={submitHandler}>
 
-            <h1 className="mx-5 my-4">shipping Address</h1>
-            {error && (
-                <Message className="mx-5 my-4" variant="danger">
-                    {error}
-                </Message>
-            )}
-            <Form.Group className="mb-3 mx-5" controlId="formBasicEmail">
-                <Form.Label>Home address</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter adress"
-                    value={address}
-                    className="sharp loginTextArea"
-                    onChange={AddresshandleChange}
-                    required={true}
-                />
-            </Form.Group>
+                <h1 className="mx-5 my-4">shipping Address</h1>
+                {error && (
+                    <Message className="mx-5 my-4" variant="danger">
+                        {error}
+                    </Message>
+                )}
+                <Form.Group className="mb-3 mx-5" controlId="formBasicEmail">
+                    <Form.Label>Home address</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter adress"
+                        value={address}
+                        className="sharp loginTextArea"
+                        onChange={AddresshandleChange}
+                        required={true}
+                    />
+                </Form.Group>
 
-            <Form.Group className="mb-3 mx-5" controlId="formBasicPassword">
-                <Form.Label>City</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="City"
-                    value={city}
-                    className="sharp loginTextArea"
-                    onChange={cityhandleChange}
-                    required={true}
-                />
-            </Form.Group>
-            <Form.Group className="mb-3 mx-5" controlId="formBasicPassword">
-                <Form.Label>phone</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="phone"
-                    value={phone}
-                    className="sharp loginTextArea"
-                    onChange={phonehandleChange}
-                    required={true}
-                />
-            </Form.Group>
-            <Button type="submit" className="sharp loginButton mx-5" variant="dark">
-                continue
-            </Button>
-        </Form>
+                <Form.Group className="mb-3 mx-5" controlId="formBasicPassword">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="City"
+                        value={city}
+                        className="sharp loginTextArea"
+                        onChange={cityhandleChange}
+                        required={true}
+                    />
+                </Form.Group>
+                <Form.Group className="mb-3 mx-5" controlId="formBasicPassword">
+                    <Form.Label>phone</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="phone"
+                        value={phone}
+                        className="sharp loginTextArea"
+                        onChange={phonehandleChange}
+                        required={true}
+                    />
+                </Form.Group>
+                <Button type="submit" className="sharp loginButton mx-5" variant="dark">
+                    continue
+                </Button>
+            </Form>
+        </>)}
+        </>
     )
 
 }
